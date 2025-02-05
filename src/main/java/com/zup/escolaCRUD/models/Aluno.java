6372f909eaf39406ad0ea6901a5427521403ab32
Eloiza <eloiza.souza@zup.com.br>
2025-02-05 11:59:05 -0300

    feat(model): add Aluno entity Created the Aluno entity to represent the students table in the database, including fields for id, name, and age.

---
+++ src/main/java/com/zup/escolaCRUD/models/Aluno.java
@@ -0,0 +1,48 @@
+package com.zup.escolaCRUD.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Aluno {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private int age;
+
+    public Aluno() {
+    }
+
+    public Aluno(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
