57a99012fb60c0bcf22c93a0ceb23d62eea508d7
Eloiza <eloiza.souza@zup.com.br>
2025-02-05 09:08:08 -0300

    feat: create Spring project

---
+++ src/test/java/com/zup/escolaCRUD/EscolaCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zup.escolaCRUD;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EscolaCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
