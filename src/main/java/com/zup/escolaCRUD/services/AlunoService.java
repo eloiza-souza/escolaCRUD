ea08e05419ec7750644d81d73c2c2191a37a8661
Eloiza <eloiza.souza@zup.com.br>
2025-02-10 14:21:05 -0300

    refactor: "create AlunoService as interface"

--- src/main/java/com/zup/escolaCRUD/services/AlunoService.java
+++ src/main/java/com/zup/escolaCRUD/services/AlunoService.java
@@ -1,33 +1,13 @@
 package com.zup.escolaCRUD.services;
 
 import com.zup.escolaCRUD.models.Aluno;
-import com.zup.escolaCRUD.repositories.AlunoRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 
 import java.util.List;
 
-@Service
-public class AlunoService {
-    @Autowired
-    private AlunoRepository alunoRepository;
+public interface AlunoService {
+    public List<Aluno> listAll();
+    public Aluno save(Aluno aluno);
+    public Aluno update(Long id, Aluno alunoAtualizado);
+    public void delete(Long id);
 
-    public List<Aluno> listAll() {
-        return alunoRepository.findAll();
-    }
-
-    public Aluno save(Aluno aluno) {
-        return alunoRepository.save(aluno);
-    }
-
-    public Aluno update(Long id, Aluno alunoAtualizado) {
-        Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
-        aluno.setName(alunoAtualizado.getName());
-        aluno.setAge(alunoAtualizado.getAge());
-        return alunoRepository.save(aluno);
-    }
-
-    public void delete(Long id) {
-        alunoRepository.deleteById(id);
-    }
 }
---
+++ src/main/java/com/zup/escolaCRUD/services/AlunoServiceImpl.java
@@ -0,0 +1,33 @@
+package com.zup.escolaCRUD.services;
+
+import com.zup.escolaCRUD.models.Aluno;
+import com.zup.escolaCRUD.repositories.AlunoRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AlunoServiceImpl implements AlunoService{
+    @Autowired
+    private AlunoRepository alunoRepository;
+
+    public List<Aluno> listAll() {
+        return alunoRepository.findAll();
+    }
+
+    public Aluno save(Aluno aluno) {
+        return alunoRepository.save(aluno);
+    }
+
+    public Aluno update(Long id, Aluno alunoAtualizado) {
+        Aluno aluno = alunoRepository.findById(id).orElseThrow(() -> new RuntimeException("Aluno não encontrado"));
+        aluno.setName(alunoAtualizado.getName());
+        aluno.setAge(alunoAtualizado.getAge());
+        return alunoRepository.save(aluno);
+    }
+
+    public void delete(Long id) {
+        alunoRepository.deleteById(id);
+    }
+}
